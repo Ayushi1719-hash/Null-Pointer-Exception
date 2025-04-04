03d75cf851667874e775d1fec9b0014fbb0ff257
Ayushi Sinha <Ayushi.Sinha@bounteous.com>
2025-04-04 11:48:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,11 @@
-const button = document.querySelector("button");
-let timer;
-let timer2;
-const audio = new Audio("https://lasonotheque.org/UPLOAD/mp3/1417.mp3");
+let button = document.querySelector("span");
+let ball = document.querySelector(".caddie__ball");
 
 button.addEventListener("click", function () {
-	if (!this.classList.contains("added")) {
-		clearTimeout(timer);
-		clearTimeout(timer2);
-	}
+	console.log("Button clicked. Current state:", this.classList.value); // <-- NEW LINE
+	this.classList.toggle("is-added");
+});
 
-	if (
-		this.classList.contains("tapis-roulant") &&
-		!this.classList.contains("added")
-	) {
-		document.querySelector(
-			".tapis-roulant>div:nth-child(4)>div"
-		).style.animationPlayState = "paused";
-		document.querySelector(
-			".tapis-roulant>div:nth-child(2)>div"
-		).style.animationPlayState = "paused";
-		this.style.pointerEvents = "none";
-		this.classList.add("canceled");
-		setTimeout(() => {
-			this.style.pointerEvents = "initial";
-			this.classList.remove("tapis-roulant");
-			this.classList.remove("canceled");
-		}, 1000);
-	}
-
-	if (!this.classList.contains("tapis-roulant")) {
-		this.classList.add("tapis-roulant");
-		document.querySelector(
-			".tapis-roulant>div:nth-child(4)>div"
-		).style.animationPlayState = "running";
-		document.querySelector(
-			".tapis-roulant>div:nth-child(2)>div"
-		).style.animationPlayState = "running";
-		timer = setTimeout(() => {
-			this.classList.add("added");
-			audio.play();
-			this.style.pointerEvents = "none";
-			timer2 = setTimeout(() => {
-				this.classList.remove("added");
-				this.classList.remove("tapis-roulant");
-				this.style.pointerEvents = "initial";
-			}, 1600);
-		}, 1400);
-	}
+ball.addEventListener("animationend", function () {
+	button.classList.remove("is-added");
 });
